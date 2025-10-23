b6b88df245a8984bf0928d35d36d3db4c7491eb9
KingDanone <lricardolv10@gmail.com>
2025-10-23 18:38:00 -0300

    feat: Add logging for spring.datasource.url at startup to debug connection issue

--- backend/projetoeco/src/main/java/com/eco/projetoeco/ProjetoecoApplication.java
+++ backend/projetoeco/src/main/java/com/eco/projetoeco/ProjetoecoApplication.java
@@ -2,12 +2,27 @@ package com.eco.projetoeco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-public class ProjetoecoApplication {
+public class ProjetoecoApplication implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProjetoecoApplication.class);
+
+    @Autowired
+    private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoecoApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Resolved spring.datasource.url: {}", env.getProperty("spring.datasource.url"));
+    }
 
 }
